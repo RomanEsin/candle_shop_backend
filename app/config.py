5ac566b7dbaa21dddfd0c36b785e152352d59fcf
Roman Esin <esinromanswift@gmail.com>
2023-06-20 03:27:30 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ DATABASE_URL = (
     "postgresql+asyncpg://your_username:your_password@postgres/your_database"
 )
 
-BACKEND_BASE = "http://localhost:8000"
+BACKEND_BASE = "https://shop.asap-it.tech"
 TELEGRAM_TOKEN = "6070396433:AAGmLqjBDNUXCg3i3KgPkITAkanKS3atH80"
